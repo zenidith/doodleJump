dbdac5db1c9d614b074fb7f28859340a086ec41c
K Sheenan <kaynesheenan+git@gmail.com>
2021-07-18 23:39:39 +0900

    updated platforms function

--- app.js
+++ app.js
@@ -1,12 +1,48 @@
-document.addEventListener('DOMContentLoaded' () => {
+document.addEventListener('DOMContentLoaded', () => {
   const grid = document.querySelector('.grid')  
   const doodler = document.createElement('div')
   let doodlerLeftSpace = 50
+  let doodlerBottomSpace = 150
+  let platformCount = 5
 
   function createDoodler() {
       grid.appendChild(doodler)
       doodler.classList.add('doodler')
       doodler.style.left = doodlerLeftSpace + 'px'
+      doodler.style.bottom = doodlerBottomSpace + 'px'
+
   }
-  createDoodler()
+
+  class Platform {
+    constructor(newPlatBottom) {
+    this.bottom = newPlatBottom  
+    this.left = Math.random() * 315
+    this.visual = document.createElement('div')
+
+    const visual = this.visual
+    visual.classList.add('platform')
+    visual.style.left = this.left + 'px'
+    visual.style.bottom = this.bottom + 'px'
+    grid.appendChild(visual)
+    }
+  }
+
+
+  function createPlatforms() {
+    for(let i =0; i < platformCount; i++) {
+      let platGap = 600 / platformCount
+      let newPlatBottom = 100 + i * platGap
+      let newPlatform = new Platform (newPlatBottom)
+      platforms.push(newPlatform)
+      console.log(platforms)
+    }
+  }
+
+  function start() {
+    if (!isGameOver) {
+      createPlatforms()
+      createDoodler()
+    } 
+  }
+  start()
 })
