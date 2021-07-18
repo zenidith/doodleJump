2720f57f103587dae916f3f23140b1f55ad5228c
K Sheenan <kaynesheenan+git@gmail.com>
2021-07-18 23:43:50 +0900

    isGameOver = false

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ document.addEventListener('DOMContentLoaded', () => {
   const doodler = document.createElement('div')
   let doodlerLeftSpace = 50
   let doodlerBottomSpace = 150
+  let isGameOver = false
   let platformCount = 5
+  let platforms = []
 
   function createDoodler() {
       grid.appendChild(doodler)
@@ -34,7 +36,6 @@ document.addEventListener('DOMContentLoaded', () => {
       let newPlatBottom = 100 + i * platGap
       let newPlatform = new Platform (newPlatBottom)
       platforms.push(newPlatform)
-      console.log(platforms)
     }
   }
 
