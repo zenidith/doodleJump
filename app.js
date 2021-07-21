099367366c699e25430f65f81f641121336f6cfd
K Sheenan <kaynesheenan+git@gmail.com>
2021-07-21 13:04:01 +0900

    images

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ document.addEventListener('DOMContentLoaded', () => {
   let isGoingRight = false
   let leftTimerId
   let rightTimerId
+  let score = 0
 
   function createDoodler() {
       grid.appendChild(doodler)
@@ -59,7 +60,9 @@ document.addEventListener('DOMContentLoaded', () => {
                   let firstPlatform = platforms[0].visual
                   firstPlatform.classList.remove('platform')
                   platforms.shift()
-                  
+                  score ++
+                  let newPlatform = new Platform(600)
+                  platforms.push(newPlatform)
               }
           })
       }
@@ -103,8 +106,14 @@ function fall() {
 
 function gameOver() {
     isGameOver = true
+    while (grid.firstChild) {
+        grid.removeChild(grid.firstChild)
+    }
+    grid.innerHTML = score
     clearInterval(upTimerId)
     clearInterval(downTimerId)
+    clearInterval(leftTimerId)
+    clearInterval(rightTimerId)
 }
 
 function control(e) {
